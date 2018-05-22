6143c9a7dc8f7ddd90f9bb81f3c9070fed41b55a
Kaz <megaoka@users.noreply.github.com>
2018-05-22 14:31:36 -0700

    Create DiscordCommands.cs

---
+++ ACT_DiscordTriggers/DiscordAPI/DiscordCommands.cs
@@ -0,0 +1,17 @@
+using Discord;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace DiscordAPI {
+  public class DiscordCommands {
+	public async Task ProcessCommand(String command)
+{
+	public string response
+	if command == "PING"
+	{
+	return "PONG"
+	}
+}
+  }
+}
